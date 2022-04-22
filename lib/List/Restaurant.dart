1486943a1818010e27bc5f244e3066d976c79d2e
NamNam2001 <tranphuongnamnaruto@gmail.com>
2022-04-22 16:18:59 +0700

    update

--- lib/List/Restaurant.dart
+++ lib/List/Restaurant.dart
@@ -31,10 +31,18 @@ List<Restaurant> restaurantList = [
    ),
    Restaurant(
     id: 3,
-    title: "Phương Nam",
-    image: "assets/images/NhaHang2.jpg",
-    price: "100.000 ₫ - 1.000.000 ₫",
+    title: "Ancient Hue Garden Houses",
+    image: "assets/images/NhaHang3.jpg",
+    price: "181.818 ₫ - 1.363.636 ₫",
     description: "Phù hợp với người ăn chay, Tùy chọn ăn chay, Tùy chọn đồ ăn không có gluten",
-    address: "55 Hàm Nghi - Huế, Việt Nam",
+    address: "104/47 Kim Long Phường Kim Long - Huế, Việt Nam",
+   ),
+   Restaurant(
+    id: 4,
+    title: "COZY Restaurant & cafe",
+    image: "assets/images/NhaHang4.jpg",
+    price: "181.818 ₫ - 1.363.636 ₫",
+    description: "Phù hợp với người ăn chay, Tùy chọn ăn chay, Tùy chọn đồ ăn không có gluten",
+    address: "104/47 Kim Long Phường Kim Long - Huế, Việt Nam",
    )
  ];
